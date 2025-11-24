c291ebe54bae678e091bcce2dab23f7ffa709e95
MoonHyeok Bae <tomo@hufs.ac.kr>
2025-11-24 22:33:06 +0900

    MFC Dialog 기반 프로젝트 생성

---
+++ GLIM/GLIM.cpp
@@ -0,0 +1,50 @@
+﻿// GLIM.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+#include "pch.h"
+#include "framework.h"
+#include "GLIM.h"
+#include "GLIMDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CGLIMApp
+
+BEGIN_MESSAGE_MAP(CGLIMApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CGLIMApp 생성
+
+CGLIMApp::CGLIMApp()
+{
+	// 다시 시작 관리자 지원
+	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
+
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CGLIMApp 개체입니다.
+
+CGLIMApp theApp;
+
+
+// CGLIMApp 초기화
+
+BOOL CGLIMApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	CGLIMDlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+
+	// 대화 상자가 닫혔으므로 응용 프로그램의 메시지 펌프를 시작하지 않고 응용 프로그램을 끝낼 수 있도록 FALSE를
+	// 반환합니다.
+	return FALSE;
+}
+
---
+++ GLIM/GLIMDlg.h
@@ -0,0 +1,32 @@
+﻿// GLIMDlg.h: 헤더 파일
+#pragma once
+
+// CGLIMDlg 대화 상자
+class CGLIMDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CGLIMDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GLIM_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+};
